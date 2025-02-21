f0649b341c58d9ae9a1be748fdd85aae1e32ee50
Joe Simmons-Talbott <josimmon@redhat.com>
2025-02-21 17:31:16 -0500

    Convert to use a JSON shares file.
    * Some minor cleanup and updates.
    * Remove all share data.

--- fh-pricer.py
+++ fh-pricer.py
@@ -1,36 +1,27 @@
 import colorama
 import finnhub
+import time
 from config import API_KEY
+
+import os
+import json
 
 finnhub_client = finnhub.Client(api_key=API_KEY)
 
-symbols = {
-    "QQQ": [
-        # (5, 496.4, None),
-        # (5, 498.6, None),
-        # (5, 499.25, None),
-        # (5, 498.25, None),
+with open(os.path.join(os.path.expanduser('~'), 'pricer.json'), 'r') as fp:
+    data = json.load(fp)
+    symbols = data['open']
+    notes = data['notes']
+    exclude_symbols = data['excludes']
 
-        (5, 474, None),
-        (5, 491.5, None),
-        (5, 496, None),
-        (5, 497, None),
-        (5, 498, None),
-    ],
-    "AVGO": [
-        (1, 1742, None),
-    ]
-}
+# Due to strict API usage limitations only query a few symbols
+symbols = {x: v for x, v in symbols.items() if x not in exclude_symbols}
 
-"""
-CARR: This was a mistake.
-    # (10, 63.1, None),
-    # (10, 63.3, None),
-AVGO:
-    (1, 1742, None),
-"""
+comp_cur_value = 0.0
+comp_gain = 0.0
 
-for symbol in symbols:
+for symbol in sorted(symbols):
+    time.sleep(0.20)
     shares = symbols[symbol]
     data = finnhub_client.quote(symbol)
     price = data['c']
@@ -46,7 +37,16 @@ for symbol in symbols:
 
     end = colorama.Style.RESET_ALL
 
-    print(f"{fore}{symbol:4} {price} {delta: .3f} {delta_percent: .3f}%{end}")
+    note = ""
+    if symbol in notes:
+        note = notes[symbol]
+
+    up = price * 1.02
+    down = price * 0.99
+    up_down = f" - {up: .2f} {down: .2f}"
+    up_down = ""
+    print(f"{symbol: <4} {fore}{price: >8.3f} {delta: .3f} "
+          f"{delta_percent: .3f}%{end} {note} {up_down}")
 
     tot_count = 0
     tot_cost = 0.0
@@ -54,20 +54,27 @@ for symbol in symbols:
     tot_percent = 0.0
     tot_value = 0.0
     tot_items = 0
+    tot_cur_value = 0.0
 
+    sym_count = 0
     for share in shares:
-        tot_items += 1
 
-        count, cost, hold = share
+        count, cost, hold, date, *rest = share
+        if rest:  # This is a sold share
+            continue
+
+        tot_items += 1
         delta = price - cost
         delta_percent = (delta / cost) * 100
         value = delta * count
+        cur_value = price * count
 
         tot_count += count
         tot_cost += cost
         tot_delta += delta
         tot_percent += delta_percent
         tot_value += value
+        tot_cur_value += cur_value
 
         if delta_percent < 0:
             fore = colorama.Fore.RED
@@ -80,8 +87,13 @@ for symbol in symbols:
         if hold is not None:
             hold_str = f"{hold}"
 
-        print(f"{fore}    {count: 4} {cost: 8.02f} {delta: >8.3f} "
-              f"{delta_percent: >8.3f}% {value: >8.3f} {hold_str}{end}")
+        tot_only = False
+
+        if not tot_only:
+            print(f"    [{sym_count: 3d}] {count: 8.3g} {fore}{cost: 8.02f} "
+                  f"{delta: >8.3f} {delta_percent: >8.3f}% {value: >8.3f}{end} "
+                  f"{hold_str: >10} {cur_value: >8.3f}")
+        sym_count += 1
 
     # Show the total values for the symbol
     if tot_value < 0:
@@ -89,9 +101,21 @@ for symbol in symbols:
     else:
         fore = colorama.Fore.GREEN
 
+    comp_cur_value += tot_cur_value
+    comp_gain += tot_value
+
     if tot_items > 1:
         print("=============================================")
-        print(f"{fore}Tot {tot_count: 4} {tot_cost / tot_items: 8.02f} "
-              f"{tot_delta / tot_items: >8.3f} "
-              f"{tot_percent / tot_items: >8.3f}% {tot_value: >8.3f}{end}")
+        print(f"Tot       {tot_count: 8.3g} {tot_cost / tot_items: 8.02f} "
+              f"{fore}{tot_delta / tot_items: >8.3f} "
+              f"{tot_percent / tot_items: >8.3f}% {tot_value: >8.3f}{end}"
+              f"  {tot_cur_value: >18.3f}")
         print()
+
+if comp_gain < 0:
+    comp_fore = colorama.Fore.RED
+else:
+    comp_fore = colorama.Fore.GREEN
+
+print(f"{comp_fore}{comp_gain: >55.3f}{end}"
+      f"{comp_cur_value: >20.3f}")
